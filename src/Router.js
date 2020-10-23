5efa5de46ebd6ddb19187eeee50da4095d019f6c
Rufaida <eng.rufaidajumaa@gmail.com>
2020-10-23 17:37:51 +0200

    Move routing code to Router file

---
+++ src/Router.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+import Home from "./components/pages/Home";
+import Gallery from "./components/pages/Gallery";
+import About from "./components/pages/About";
+// import ErrorPage from "./components/pages/ErrorPage";
+import Content from "./components/pages/Content";
+import "./App.css";
+import VisitUs from "./components/pages/VisitUs";
+
+function App() {
+  return (
+    <div className="App">
+
+ <Router>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/content" component={Content} />
+    <Route path="/gallery" component={Gallery} />
+    <Route path="/contact" component={VisitUs} />
+    <Route path="/about" component={About} />
+    {/* <Route component={ErrorPage} /> */}
+  </Switch>
+</Router>
+    </div>
+  );
+}
+
+export default App;
+
