d635122d403f527f5364c534cea45cbf12ba4414
Rufaida <eng.rufaidajumaa@gmail.com>
2020-10-23 16:37:41 +0200

    Add MainPage component

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function MainPage() {
+return(
+    <div className="main-page">
+        <div className="resturant-name">
+        <h1>ARABI FOOD</h1>
+          <p>
+            <span>Syrisk </span>  
+            <span>tapas </span> 
+            <span>lavet </span>
+            <span>med </span>
+            <span>passion </span>
+            <span>og </span>
+            <span>kærlighed </span>
+            </p>
+      </div>
+    </div>
+) 
+}
+export default MainPage;
