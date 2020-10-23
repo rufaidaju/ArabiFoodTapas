c0b4387056886e56f210a3f4ecb6623df7456f57
Rufaida <eng.rufaidajumaa@gmail.com>
2020-10-24 00:17:45 +0200

    Add resturant image

--- src/components/pages/VisitUs.js
+++ src/components/pages/VisitUs.js
@@ -6,13 +6,15 @@ import address from "../../assets/icons/location-pin.png";
 import facebook from "../../assets/icons/facebook-blue.png";
 import time from "../../assets/icons/time.png";
 import cvr from "../../assets/icons/cvr.png";
+import reemRestaurant1 from "../../assets/images/ReemRestaurant1.jpg";
+
 
 function VisitUs() {
   return (
-    <section id="contact-us" className="visitUs">
+    <section className="visitUs">
       <Map />
 
-      <h1>Kontakt os</h1>
+      <h2>Kontakt os</h2>
 
       <div className="contact-container">
         <div className="cont">
@@ -26,7 +28,7 @@ function VisitUs() {
             <div className="contact-info">
               <img src={`${address}`} alt="" />
               <a href="https://g.page/VaerftetsMadmarked?share" target="_blank" rel="noopener noreferrer">
-                <p>Værftets Madmarked, Ny Kronborgvej 2, 3000 Helsingør</p>
+                <p>Ny Kronborgvej 2, 3000 Helsingør</p>
               </a>
             </div>
             <div className="contact-info">
@@ -63,6 +65,9 @@ function VisitUs() {
               <p> 39447630</p>
             </div>
           </div>
+        </div>
+        <div className="resturant-img">
+        <img src={`${reemRestaurant1}`} alt="Arabi Food resturant" />
         </div>
       </div>
     </section>
