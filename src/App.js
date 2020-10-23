bfe2b2babb981059b8530889db9f8c50545e071b
Rufaida <eng.rufaidajumaa@gmail.com>
2020-10-23 17:38:53 +0200

    Import router to App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import NavigationBar from "./components/Navbar";
-import Home from "./components/pages/Home";
-import Gallery from "./components/pages/Gallery";
-import About from "./components/pages/About";
-// import ErrorPage from "./components/pages/ErrorPage";
-import Footer from "./components/Footer";
-import "./App.css";
-import VisitUs from "./components/pages/VisitUs";
-
-function App() {
-  return (
-    <div className="App">
-
- <Router>
-  <NavigationBar />
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/gallery" component={Gallery} />
-    <Route path="/contact" component={VisitUs} />
-    <Route path="/about" component={About} />
-    {/* <Route component={ErrorPage} /> */}
-  </Switch>
-</Router>
-<Footer /> 
+import React ,  { Component } from 'react';
+import Router from './Router';
 
 
-    </div>
-  );
-}
+class App extends Component {
+  
+ render() {
+   return (
+        <div >
+          <Router />
+          
+        </div>
+      );
+    }
+  }
+
 
 export default App;
-
