31e389bee9c8ea84a271ebe49494b19b6b3cc0ca
myshuker <myshuker@gmai.com>
2020-10-24 16:56:00 +0200

    responsive design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,33 +3,55 @@ import React from "react";
 function Footer() {
   return (
     <section className="footer">
-      <p>
-        <a href="https://www.hackyourfuture.dk/ " target="_blank" rel="noopener noreferrer" >
-          HackYourFuture{" "}
-        </a>
-        <span> &copy; </span> 2020 All rights reserved. Partnered with{" "}
-        <span>
-          <a href="https://www.finklusiv.dk/" target="_blank" rel="noopener noreferrer">
-            {" "}
-            Finklusiv.dk{" "}
+      <div>
+        <p>
+          <a
+            href="https://www.hackyourfuture.dk/ "
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            HackYourFuture{" "}
           </a>
-        </span>{" "}
-        .<div></div>
-        Designed By developed by{" "}
-        <span>
-          <a href="https://myshuker.github.io/myPortfolio/#/ " target="_blank" rel="noopener noreferrer">
-            {" "}
-            Mohammad Alshoker{" "}
-          </a>
-        </span>
-        and{" "}
-        <span>
-          <a href="https://rufaidaju.github.io/Rufaida-portfolio/" target="_blank" rel="noopener noreferrer">
-            {" "}
-            Rufaida Jumaa{" "}
-          </a>
-        </span>
-      </p>
+          <span> &copy; </span> 2020 All rights reserved. Partnered with{" "}
+          <span>
+            <a
+              href="https://www.finklusiv.dk/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Finklusiv.dk{" "}
+            </a>
+          </span>{" "}
+        </p>
+      </div>
+
+      <div>
+        <p>
+          Designed By developed by{" "}
+          <span>
+            <a
+              href="https://myshuker.github.io/myPortfolio/#/ "
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Mohammad Alshoker{" "}
+            </a>
+          </span>
+          and{" "}
+          <span>
+            <a
+              href="https://rufaidaju.github.io/Rufaida-portfolio/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "} 
+              Rufaida Jumaa{" "}
+            </a>
+          </span>
+        </p>
+      </div>
     </section>
   );
 }
