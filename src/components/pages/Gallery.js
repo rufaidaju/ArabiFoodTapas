0914a47db47ab9c31b1209cf87b1ed9154be287f
Rufaida <eng.rufaidajumaa@gmail.com>
2020-10-25 09:38:49 +0100

    Edit the gallery style and images

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,17 +1,9 @@
 import React from "react";
 
-import reem1 from "../../assets/images/Reem1.jpeg";
 import reem2 from "../../assets/images/Reem2.jpeg";
 import reem3 from "../../assets/images/Reem3.jpeg";
-// import reem4 from "../../assets/images/Reem4.jpeg";
-import reem5 from "../../assets/images/Reem5.jpeg";
 import reem6 from "../../assets/images/Reem6.jpeg";
-import reem7 from "../../assets/images/Reem7.jpg";
-import reemRestaurant1 from "../../assets/images/ReemRestaurant1.jpg";
-import reemRestaurant2 from "../../assets/images/ReemRestaurant2.jpg";
-import reemRestaurant3 from "../../assets/images/ReemRestaurant3.jpg";
 import food1 from "../../assets/images/food1.jpg";
-import food2 from "../../assets/images/food2.jpg";
 import food3 from "../../assets/images/food3.jpg";
 import food4 from "../../assets/images/food4.jpg";
 import food5 from "../../assets/images/food5.jpg";
@@ -21,39 +13,35 @@ import food8 from "../../assets/images/food8.jpg";
 import food9 from "../../assets/images/food9.jpg";
 import kitchen from "../../assets/images/kitchen.jpg";
 import lovedamas from "../../assets/images/lovedamas.jpg";
-import tapas from "../../assets/images/tapas.png";
+import reemRestaurant3 from "../../assets/images/ReemRestaurant3.jpg";
+import food10 from "../../assets/images/image6.jpg";
+import food11 from "../../assets/images/food2.jpg";
+import food12 from "../../assets/images/menuList.jpg";
 
 function Gallery() {
   return (
     <section className="gallery">
       <h2>Galleri</h2>
-      <p>
-        
-      </p>{" "}
       <div className="gallery-container">
         <div className="gallery-group ">
-          <img src={`${reem1}`} alt="" />
           <img src={`${reem2}`} alt="" />
           <img src={`${reem3}`} alt="" />
-          <img src={`${reem5}`} alt="" />
           <img src={`${reem6}`} alt="" />
-          <img src={`${reem7}`} alt="" />
+          <img src={`${food8}`} alt="" />
           <img src={`${food1}`} alt="" />
-          <img src={`${food2}`} alt="" />
           <img src={`${food3}`} alt="" />
           <img src={`${food4}`} alt="" />
           <img src={`${food5}`} alt="" />
           <img src={`${food6}`} alt="" />
           <img src={`${food7}`} alt="" />
-          <img src={`${food8}`} alt="" />
           <img src={`${food9}`} alt="" />
           <img src={`${kitchen}`} alt="" />
-
           <img src={`${lovedamas}`} alt="" />
-          <img src={`${reemRestaurant2}`} alt="" />
           <img src={`${reemRestaurant3}`} alt="" />
-
-          <img src={`${tapas}`} alt="" />
+          <img src={`${food10}`} alt="" />
+          <img src={`${food11}`} alt="" />
+          <img src={`${food12}`} alt="" />
+          
         </div>
       </div>
     </section>
